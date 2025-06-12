3f81b42f06261c8f273f55e9c575ed412c220ee3
Diana Butiakova <butiakova.diana@gmail.com>
2025-06-13 00:24:53 +0600

    feat: create functions working with users array

---
+++ src/data/users.ts
@@ -0,0 +1,68 @@
+import { randomUUID } from 'crypto';
+
+type user = {
+    id: string;
+    username: string;
+    age: number;
+    hobbies: string[];
+};
+
+const users: user[] = [
+    {
+        id: randomUUID(),
+        username: 'skywalker92',
+        age: 31,
+        hobbies: ['reading', 'cycling', 'chess'],
+    },
+    {
+        id: randomUUID(),
+        username: 'pixelFox',
+        age: 24,
+        hobbies: ['gaming', 'drawing', 'streaming'],
+    },
+    {
+        id: randomUUID(),
+        username: 'byteHunter',
+        age: 28,
+        hobbies: ['coding', 'hiking', 'photography'],
+    },
+    {
+        id: randomUUID(),
+        username: 'novaPulse',
+        age: 22,
+        hobbies: ['music', 'writing', 'traveling'],
+    },
+    {
+        id: randomUUID(),
+        username: 'codeCactus',
+        age: 35,
+        hobbies: ['gardening', 'blogging', 'puzzles'],
+    },
+];
+
+const getUsers = () => users;
+const getUser = (idToFind: string) => users.find(({ id }) => id === idToFind);
+const addUser = (newUser: user) => {
+    users.push(newUser);
+    return { ...newUser };
+};
+const updateUser = (userToUpdate: user) => {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i]?.id === userToUpdate.id) {
+            users[i] = userToUpdate;
+            return { ...users[i] };
+        }
+    }
+
+    addUser(userToUpdate);
+};
+const deleteUser = (idToDelete: string) => {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i]?.id === idToDelete) {
+            users.splice(i, 1);
+            return [...users];
+        }
+    }
+};
+
+export { getUsers, getUser, addUser, updateUser, deleteUser };
